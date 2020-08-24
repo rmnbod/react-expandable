f6506a53925fa27a24b58ad0ca09ac24bcf94224
Roman Bodnarchuk <r.bodnarchuk@electroeffect.com.ua>
2020-08-24 19:05:00 +0300

    Fix expand icon, decrease the size by 1px

--- src/accordion/item/assets/expand-ic.tsx
+++ src/accordion/item/assets/expand-ic.tsx
@@ -6,7 +6,7 @@ type Props = { isExpanded: boolean }
 export function ExpandIcon(props: Props) {
     return (
         <svg width="20px" height="20px" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="10" cy="10" r="10"/>
+            <circle cx="10" cy="10" r="9"/>
             <line x1="6" y1="10" x2="14" y2="10" stroke="white" strokeWidth="1.5" strokeLinecap="round"/>
             <line x1="10" y1="6" x2="10" y2="14" stroke="white" strokeWidth="1.5" strokeLinecap="round"
                   className={ props.isExpanded ? styles.collapsed : styles.expanded }
