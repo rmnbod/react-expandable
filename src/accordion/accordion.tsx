fbec563fd22e3bba2ab5c83370c4f7e8fbff4044
Roman Bodnarchuk <r.bodnarchuk@electroeffect.com.ua>
2020-08-24 19:00:55 +0300

    Add title & Quicksand font

--- src/accordion/accordion.tsx
+++ src/accordion/accordion.tsx
@@ -8,6 +8,7 @@ type Props = {tocItems: TocItem[]}
 export function Accordion(props: Props) {
     return (
         <div className={styles.container}>
+            <h1>Table of Contents</h1>
             {props.tocItems.map(tocItem => {
                 return (
                    <Item tocItem={tocItem}/>
