4ff8640e03ca0adffdafd5a00aaaa53894c094e5
Roman Bodnarchuk <r.bodnarchuk@electroeffect.com.ua>
2020-08-29 15:31:03 +0300

    Refactor accordion modal window

--- src/accordion/accordion.tsx
+++ src/accordion/accordion.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import styles from './accordion.module.sass'
 import { TocItem } from "./model/toc-item";
-import { Item } from "./item/item";
+import { Heading } from "./heading/heading";
+import { Content } from "./content/content";
+import styles from "./accordion.module.sass";
 
-type Props = {tocItems: TocItem[]}
+type Props = { tocItems: TocItem[] }
 
 export function Accordion(props: Props) {
     return (
         <div className={styles.container}>
-            <h1>Table of Contents</h1>
-            {props.tocItems.map(tocItem => {
-                return (
-                   <Item tocItem={tocItem}/>
-                )
-            })}
+            <Heading/>
+            <Content tocItems={ props.tocItems }/>
         </div>
     )
 }
